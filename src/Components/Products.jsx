f7b716b7eb4290659bfda1ef93cd856a548a9136
AhmedYeasin <ahmetchatgami@gmail.com>
2026-07-06 22:02:24 +0600

    product added successfully

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,11 +1,11 @@
 export default function Products({ products }) {
     console.log(products)
-    return <div>
+    return <div className="font-roboto">
         <h2>Our Products</h2>
-        <div>
+        <div className="flex flex-wrap gap-4 justify-center">
             {products.map(product => {
                 return (
-                    <div key={product.id} className="card bg-base-100 w-96 shadow-sm">
+                    <div key={product.id} className="card bg-base-white w-96 shadow-sm outline outline-1 outline-primary-content flex">
                         <figure>
                             <img
                                 src={product.image}
@@ -13,9 +13,9 @@ export default function Products({ products }) {
                         </figure>
                         <div className="card-body">
                             <h2 className="card-title">{product.name}</h2>
-                            <p>{product.description}</p>
+                            <p>{product.short_details}</p>
                             <div className="card-actions justify-end">
-                                <button className="btn btn-primary">Buy Now</button>
+                                <button className="btn btn-accent">Buy Now</button>
                             </div>
                         </div>
                     </div>
