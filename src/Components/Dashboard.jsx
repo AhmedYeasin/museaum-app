0fd3f4fc36761d4f4fd064ad30436a2baacdec42
AhmedYeasin <ahmetchatgami@gmail.com>
2026-07-09 23:05:03 +0600

    wanted and unwanted Items can load on dashboard

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,5 +1,44 @@
+import { useLoaderData } from "react-router"
+import { getUnwantedIdeasID, getWantedIdeasID } from "../localstorage.js"
+import { BoxIcon, Heart, ThumbsDown } from "lucide-react"
+
 export default function Dashboard(){
+    
+    const products = useLoaderData()
+
+    const wantedIds = getWantedIdeasID()
+    const unwantedIds = getUnwantedIdeasID()
+
+    const wantedCount = wantedIds.length;
+    const unwantedCount = unwantedIds.length;
+
+
     return (
-        <div>Dashboard</div>
+        <div className= "px-6 my-10 space-y-8">
+            <h1 className="italic font-news text-3xl bg-white p-4 font-bold">Dashboard</h1>
+        <div className="flex gap-8">
+            <div className="stat rounded-box">
+                <div className="stat-figure">
+                    <Heart/>
+                </div>
+                <div className="stat-title">Wanted Items</div>
+                <div className="stat-value">{wantedCount}</div>
+            </div>
+            <div className="stat rounded-box">
+                <div className="stat-figure">
+                    <ThumbsDown/>
+                </div>
+                <div className="stat-title">Unwanted Items</div>
+                <div className="stat-value">{wantedCount}</div>
+            </div>
+            <div className="stat rounded-box">
+                <div className="stat-figure">
+                    <BoxIcon/>
+                </div>
+                <div className="stat-title">Total Items</div>
+                <div className="stat-value">{products.length}</div>
+            </div>
+        </div>
+        </div>
     )
 }
