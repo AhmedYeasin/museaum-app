c3996d28ac5d67b6743c1b6fc0619b23c923d9c4
AhmedYeasin <ahmetchatgami@gmail.com>
2026-07-10 11:55:06 +0600

    barchart created on Products overview

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,9 +1,11 @@
 import { useLoaderData } from "react-router"
 import { getUnwantedIdeasID, getWantedIdeasID } from "../localstorage.js"
 import { BoxIcon, Heart, ThumbsDown } from "lucide-react"
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
 
-export default function Dashboard(){
-    
+
+export default function Dashboard() {
+
     const products = useLoaderData()
 
     const wantedIds = getWantedIdeasID()
@@ -12,33 +14,46 @@ export default function Dashboard(){
     const wantedCount = wantedIds.length;
     const unwantedCount = unwantedIds.length;
 
+    const barData = [...products].sort((a, b) => b.views - a.views).slice(0, 7).map(p => ({ name: p.name, views: p.views }))
 
     return (
-        <div className= "px-6 my-10 space-y-8">
+        <div className="px-6 my-10 space-y-8">
             <h1 className="italic font-news text-3xl bg-white p-4 font-bold">Dashboard</h1>
-        <div className="flex gap-8">
-            <div className="stat rounded-box">
-                <div className="stat-figure">
-                    <Heart/>
+            <div className="flex gap-8">
+                <div className="stat rounded-box">
+                    <div className="stat-figure">
+                        <Heart />
+                    </div>
+                    <div className="stat-title">Wanted Items</div>
+                    <div className="stat-value">{wantedCount}</div>
                 </div>
-                <div className="stat-title">Wanted Items</div>
-                <div className="stat-value">{wantedCount}</div>
-            </div>
-            <div className="stat rounded-box">
-                <div className="stat-figure">
-                    <ThumbsDown/>
+                <div className="stat rounded-box">
+                    <div className="stat-figure">
+                        <ThumbsDown />
+                    </div>
+                    <div className="stat-title">Unwanted Items</div>
+                    <div className="stat-value">{wantedCount}</div>
                 </div>
-                <div className="stat-title">Unwanted Items</div>
-                <div className="stat-value">{wantedCount}</div>
-            </div>
-            <div className="stat rounded-box">
-                <div className="stat-figure">
-                    <BoxIcon/>
+                <div className="stat rounded-box">
+                    <div className="stat-figure">
+                        <BoxIcon />
+                    </div>
+                    <div className="stat-title">Total Items</div>
+                    <div className="stat-value">{products.length}</div>
                 </div>
-                <div className="stat-title">Total Items</div>
-                <div className="stat-value">{products.length}</div>
             </div>
-        </div>
+
+            <div className="pt-4">
+                <h2 className="font-news font-semibold italic text-xl p-4">Products Overview:</h2>
+
+                <ResponsiveContainer height={300}>
+                    <BarChart data={barData}>
+                        <XAxis dataKey={"name"}/>
+                        <YAxis tick={{fontSize: 12}}/>
+                        <Bar dataKey={"views"} fill="maroon"/>
+                    </BarChart>
+                </ResponsiveContainer>
+            </div>
         </div>
     )
 }
