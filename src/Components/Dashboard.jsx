cd8ad3ae0a5bd404798023726158995cfca26753
AhmedYeasin <ahmetchatgami@gmail.com>
2026-07-10 22:40:56 +0600

    Pie chart shows total wanted and unwanted data collections

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useLoaderData } from "react-router"
 import { getUnwantedIdeasID, getWantedIdeasID } from "../localstorage.js"
 import { BoxIcon, Heart, ThumbsDown } from "lucide-react"
-import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts"
+import { Bar, BarChart, PieChart, Legend, Tooltip, ResponsiveContainer, XAxis, YAxis, Pie } from "recharts"
 
 
 export default function Dashboard() {
@@ -16,34 +16,54 @@ export default function Dashboard() {
 
     const barData = [...products].sort((a, b) => b.views - a.views).slice(0, 7).map(p => ({ name: p.name, views: p.views }))
 
+    const pieData = [
+        {
+            name: "Wanted",
+            value: wantedCount,
+            fill: "blue",
+        },
+        {
+            name: "Unwanted",
+            value: unwantedCount,
+            fill: "red",
+        },
+        {
+            name: "Total",
+            value: products.length,
+            fill: "green",
+        },
+    ]
+
     return (
         <div className="px-6 my-10 space-y-8">
             <h1 className="italic font-news text-3xl bg-white p-4 font-bold">Dashboard</h1>
             <div className="flex gap-8">
                 <div className="stat rounded-box border shadow-xl">
-                    <div className="stat-figure">
-                        <Heart />
+                    <div className="stat-figure text-blue-700 hover:fill-blue">
+                        <Heart/>
                     </div>
                     <div className="stat-title">Wanted Items</div>
                     <p className="stat-value">{wantedCount}</p>
                 </div>
                 <div className="stat rounded-box border shadow-xl">
                     <div className="stat-figure">
-                        <ThumbsDown />
+                        <ThumbsDown fill="red" />
                     </div>
                     <div className="stat-title">Unwanted Items</div>
-                    <div className="stat-value">{wantedCount}</div>
+                    <div className="stat-value">{unwantedCount}</div>
                 </div>
                 <div className="stat rounded-box border shadow-xl">
-                    <div className="stat-figure">
-                        <BoxIcon />
+                    <div className="stat-figure text-green-700">
+                        <BoxIcon fill={"green"}/>
                     </div>
                     <div className="stat-title">Total Items</div>
                     <div className="stat-value">{products.length}</div>
                 </div>
             </div>
 
-            <div className="pt-4">
+            {/* Bar chart data */}
+
+            <div className="pt-4 border-b">
                 <h2 className="font-news font-semibold italic text-xl p-4">Products Overview:</h2>
 
                 <ResponsiveContainer height={300}>
@@ -55,6 +75,20 @@ export default function Dashboard() {
                             radius={[4, 4, 0, 0]} />
                     </BarChart>
                 </ResponsiveContainer>
+            </div>
+            
+            {/* Pie Chart Data */}
+            <div className="pt-4">
+                <h2 className="font-news font-semibold italic text-xl p-4 text-center">Choose Overview</h2>
+
+                <ResponsiveContainer height={300}>
+                    <PieChart data={barData}>
+                        <Pie data={pieData}></Pie>
+                        <Tooltip/>
+                        <Legend/>
+                    </PieChart>
+                </ResponsiveContainer>
+                <p className="text-center p-2"><span className="bg-white p-1.5 text-xs text-shadow"> This Pie shows collective data of which I like to get or not of total products.</span></p>
             </div>
         </div>
     )
