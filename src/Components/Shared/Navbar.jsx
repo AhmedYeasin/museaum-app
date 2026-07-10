0443a949d9299483d6d4b41f0be8233bfe2a10ce
AhmedYeasin <ahmetchatgami@gmail.com>
2026-07-10 23:02:54 +0600

    dark mood setup controlled and saved in local storage

--- src/Components/Shared/Navbar.jsx
+++ src/Components/Shared/Navbar.jsx
@@ -1,6 +1,9 @@
+import { Moon, Sun } from "lucide-react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router"
 
 export default function Navbar() {
+    const [theme, setTheme] = useState("caramellatte")
 
     const links = (
         <>
@@ -10,6 +13,19 @@ export default function Navbar() {
         </>
     )
 
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme");
+        setTheme(savedTheme)
+        document.documentElement.setAttribute("data-theme", savedTheme)
+    }, [])
+
+    const toggleMood = () => {
+        const nextTheme = theme === "dracula" ? "caramellate" : "dracula"
+        setTheme(nextTheme);
+        localStorage.setItem("theme", nextTheme);
+        document.documentElement.setAttribute("data-theme", nextTheme)
+    }
+
     return (
         <nav className="navbar shadow-sm p-6">
             <Link to={"/"} className="text-xl font-bold font-news navbar-start">Fusion Museum</Link>
@@ -18,28 +34,38 @@ export default function Navbar() {
                 {links}
             </ul>
 
-            <div className="dropdown dropdown-end navbar-end">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                        <img
-                            alt="Tailwind CSS Navbar component"
-                            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                    </div>
-                </div>
-                <ul
-                    tabIndex="-1"
-                    className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                    <li>
-                        <a className="justify-between">
-                            Profile
-                            <span className="badge">New</span>
-                        </a>
-                    </li>
-                    <li><a href="/dashboard">Dashboard</a></li>
-                    <li><a>Settings</a></li>
-                    <li><a>Logout</a></li>
-                </ul>
+            <div className="navbar-end">
+
+
             </div>
+            <div className="navbar-end">
+                <button onClick={toggleMood} className="btn btn-ghost">
+                    {theme === "dracula" ? <Sun /> : <Moon />}
+                </button>
+                <div className="dropdown dropdown-end">
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img
+                                alt="Tailwind CSS Navbar component"
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        </div>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a href="/dashboard">Dashboard</a></li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+
         </nav>
     )
 }
